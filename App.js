5f98bf9c036a92ec62e47b106410e758ccfa5596
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-20 10:48:57 +0500

    Add two basic components shell

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddDeck from './components/AddDeck';
+import DeckListView from './components/DeckListView';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        {/* <AddDeck /> */}
+        <DeckListView />
       </View>
     );
   }
