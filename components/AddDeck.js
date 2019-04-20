7d0cd81e198c298064490863dad5b10724b3c537
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-20 12:39:12 +0500

    add some controls to add deck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { black, white } from '../utils/colors';
 
 export default class AddDeck extends Component {
+  state = {
+    input: ''
+  }
   render() {
+    const { input } = this.state
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Add Deck</Text>
+        <Text>What is the title of your new deck?</Text>
+        <View style={{padding: 10, borderColor: black, borderWidth: 2}}>
+          <TextInput
+          />
+        </View>
+        <TouchableOpacity style={{backgroundColor: black }}>
+          <Text style={{color: white}}>Submit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
