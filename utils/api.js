405c7ca69dfff387d682507eaf2ee95952681ffc
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-27 06:13:30 +0500

    improve AsynStorage based API

--- utils/api.js
+++ utils/api.js
@@ -1,24 +1,52 @@
 import { AsyncStorage } from 'react-native'
 export const DECKS_STORAGE_KEY = 'ReactNDFlashCards:decks'
 
-export function fetchDecks () {
+export function getDecks () {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+          .then( (results) => {
+            return JSON.parse(results)
+          })
 }
 
-export function submitDeck ({ deck, key }) {
+export function getDeck (id) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+          .then( (results) => {
+            const data = JSON.parse(results);
+            return data[id]
+          })
+}
+
+export function saveDeckTitle (title) {
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
-    [key]: deck
-  }))
+            [title]: { title: title, questions: [] }
+          }))
+}
+
+export function saveDeck ({ title, deck }) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+            [title]: deck
+          }))
 }
 
 export function removeDeck (key) {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
-    .then((results) => {
+          .then((results) => {
+            const data = JSON.parse(results)
+            data[key] = undefined
+            delete data[key]
+            AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
+          })
+}
+
+export function addCardToDeck(title, card) {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+  .then((results) => {
       const data = JSON.parse(results)
-      data[key] = undefined
-      delete data[key]
-      AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
-    })
+      if(data[title] !== undefined) {
+          data[title].questions.push(card);
+          AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+      }
+  })
 }
 
 function generateUID () {
